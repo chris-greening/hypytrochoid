95f91aeace63be359714412f9881bdb1d1fb8c92
Christopher Greening <chris@christophergreening.com>
2023-03-05 11:42:29 -0500

    ADD: added missing __init__.py file

--- test2.py
+++ test2.py
@@ -3,24 +3,14 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+from hypotrochoid import Hypotrochoid
+
 R = 200
 r = 122.5
 d = 300
-
-def x(theta: int) -> int:
-    """Return calculated value of parametrized x equation"""
-    return (R - r)*math.cos(theta) + d*math.cos(((R-r)/r)*theta)
-
-def y(theta: int) -> int:
-    """Return calculated value of parametrized x equation"""
-    return (R - r)*math.sin(theta) + d*math.sin(((R-r)/r)*theta)
-
-# Vectorize equations 
-vx = np.vectorize(x)
-vy = np.vectorize(y)
-
 thetas = np.arange(0,360, .005)
-x_vals = vx(thetas)
-y_vals = vy(thetas)
-plt.plot(x_vals, y_vals, linewidth=.2)
+
+test_hypotrochoid = Hypotrochoid(R, r, d, thetas)
+
+plt.plot(test_hypotrochoid.x, test_hypotrochoid.y, linewidth=.2)
 plt.show()
