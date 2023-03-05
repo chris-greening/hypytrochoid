b750509a71cd8c6aa533094b5f0d0b859ad8da58
Christopher Greening <chris@christophergreening.com>
2023-03-05 11:41:04 -0500

    MODIFY: changed test script

--- test2.py
+++ test2.py
@@ -7,7 +7,13 @@ R = 200
 r = 122.5
 d = 300
 
+def x(theta: int) -> int:
+    """Return calculated value of parametrized x equation"""
+    return (R - r)*math.cos(theta) + d*math.cos(((R-r)/r)*theta)
 
+def y(theta: int) -> int:
+    """Return calculated value of parametrized x equation"""
+    return (R - r)*math.sin(theta) + d*math.sin(((R-r)/r)*theta)
 
 # Vectorize equations 
 vx = np.vectorize(x)
