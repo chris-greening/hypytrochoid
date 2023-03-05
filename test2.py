c74713c546cbbc6331899693d3dc9277043f4c09
Christopher Greening <chris@christophergreening.com>
2023-03-05 11:45:18 -0500

    MODIFY: changed name of var

--- test2.py
+++ test2.py
@@ -10,7 +10,7 @@ r = 122.5
 d = 300
 thetas = np.arange(0,360, .005)
 
-test_hypotrochoid = Hypotrochoid(R, r, d, thetas)
+hypotrochoid = Hypotrochoid(R, r, d, thetas)
 
-plt.plot(test_hypotrochoid.x, test_hypotrochoid.y, linewidth=.2)
+plt.plot(hypotrochoid.x, hypotrochoid.y, linewidth=.2)
 plt.show()
