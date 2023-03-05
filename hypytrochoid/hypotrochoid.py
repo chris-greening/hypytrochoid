51afd8eb0c07291ff0a0ab3d82c756c3f78b59c4
Christopher Greening <chris@christophergreening.com>
2023-03-05 11:40:25 -0500

    REFACTOR: moved Hypotrochoid class into its own moduel

--- hypytrochoid/hypotrochoid.py
+++ hypytrochoid/hypotrochoid.py
@@ -1,2 +1,21 @@
 import math
 
+class Hypotrochoid:
+    """Model of a hypotrochoid"""
+    def __init__(self, R, r, d, thetas) -> None:
+        self.R = R
+        self.r = r
+        self.d = d
+        self.thetas = thetas 
+    
+        self.x = [calculate_x(R, r, d, theta) for theta in self.thetas]
+        self.y = [calculate_y(R, r, d, theta) for theta in self.thetas]
+
+def calculate_x(R: float, r: float, d: flaot, theta: float) -> float:
+    """Return calculated x-value from parametrized equation"""
+    return (R - r)*math.cos(theta) + d*math.cos(((R-r)/r)*theta)
+
+def calculate_y(R: float, r: float, d: flaot, theta: float) -> float:
+    """Return calculated y-value from parametrized equation"""
+    return (R - r)*math.sin(theta) + d*math.sin(((R-r)/r)*theta)
+
--- test2.py
+++ test2.py
@@ -7,24 +7,7 @@ R = 200
 r = 122.5
 d = 300
 
-class Hypotrochoid:
-    """Model of a hypotrochoid"""
-    def __init__(self, R, r, d, thetas) -> None:
-        self.R = R
-        self.r = r
-        self.d = d
-        self.thetas = thetas 
-    
-        self.x = [calculate_x(R, r, d, theta) for theta in self.thetas]
-        self.y = [calculate_y(R, r, d, theta) for theta in self.thetas]
 
-def calculate_x(R: float, r: float, d: flaot, theta: float) -> float:
-    """Return calculated x-value from parametrized equation"""
-    return (R - r)*math.cos(theta) + d*math.cos(((R-r)/r)*theta)
-
-def calculate_y(R: float, r: float, d: flaot, theta: float) -> float:
-    """Return calculated y-value from parametrized equation"""
-    return (R - r)*math.sin(theta) + d*math.sin(((R-r)/r)*theta)
 
 # Vectorize equations 
 vx = np.vectorize(x)
